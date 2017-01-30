432aad48515755e45d35bd8d8a0b712037bbcd64
Josh Cox <josh@webhosting.coop>
2017-01-30 12:39:54 -0600

    killmongo

--- killmongo.sh
+++ killmongo.sh
@@ -3,7 +3,6 @@ MONGOPID=mongopid
 if [ -d "$MONGOPID" ]; then
     # Control will enter here if $MONGOPID exists.
     echo 'Killing MongoDB'
-    cp -a $MONGOPID ./
-    sh "kill -HUP `cat mongopid`"
+    kill -HUP `cat mongopid`
+    rm mongopid
 fi
-rm mongopid

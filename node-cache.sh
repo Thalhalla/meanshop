4f9e8af5ad2c7d558c1107771b1c1923fa2f05a2
Josh Cox <josh@webhosting.coop>
2017-01-29 21:16:27 -0600

    rsync

--- node-cache.sh
+++ node-cache.sh
@@ -3,5 +3,5 @@ DIRECTORY=/var/jenkins_home/
 if [ -d "$DIRECTORY" ]; then
     # Control will enter here if $DIRECTORY exists.
     echo 'caching node_modules'
-    #rsync -av node_modules $DIRECTORY/
+    rsync -av node_modules $DIRECTORY/
 fi

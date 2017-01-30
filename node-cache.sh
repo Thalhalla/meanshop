b1855832ff6396d0af7ff80c01181c45ee74876f
Josh Cox <josh@webhosting.coop>
2017-01-29 20:49:23 -0600

    echoes

--- node-cache.sh
+++ node-cache.sh
@@ -2,5 +2,6 @@
 DIRECTORY=/var/jenkins_home/
 if [ -d "$DIRECTORY" ]; then
     # Control will enter here if $DIRECTORY exists.
+    echo 'caching node_modules'
     rsync -av node_modules $DIRECTORY/
 fi
--- node-sync.sh
+++ node-sync.sh
@@ -2,5 +2,6 @@
 DIRECTORY=/var/jenkins_home/node_modules
 if [ -d "$DIRECTORY" ]; then
     # Control will enter here if $DIRECTORY exists.
+    echo 'Found Cache'
     cp -a $DIRECTORY ./
 fi

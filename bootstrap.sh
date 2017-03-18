04650fafe57d7121638fe16fd3cb1bfde300feb3
Josh Cox <josh@webhosting.coop>
2017-03-18 14:54:58 -0500

    bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+: ${BRANCH=master}
+
+cd /meanshop
+if [ -d ".git" ]; then
+    git reset --hard HEAD
+    git fetch --prune http
+    git checkout $BRANCH
+else
+    git init
+    git remote add http https://github.com/Thalhalla/meanshop.git
+    git fetch --prune http
+    git checkout $BRANCH
+fi
+
+npm install
+bower install
+npm start

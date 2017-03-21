eea33b02554dd5218372ec2392746154d8e7674a
Josh Cox <josh@webhosting.coop>
2017-03-21 09:30:23 -0500

    debug

--- node/start.sh
+++ node/start.sh
@@ -5,4 +5,13 @@ if [ ! -z ${MEANGO_HOST+x} ]
     echo "$MEANGO_HOST meango.meangohost.com meango">>/etc/hosts
 fi
 
+if [ ! -z ${MEANGO_DEBUG+x} ]
+  then
+    echo '>>>>>>>>>>>>>>DEBUG<<<<<<<<<<<<<<<<<<<<'
+    env
+    cat /etc/hosts
+    echo "meango host = $MEANGO_HOST"
+    echo '>>>>>>>>>>>>END_DEBUG<<<<<<<<<<<<<<<<<<'
+fi
+
 npm start

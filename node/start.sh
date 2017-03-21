64362c9ec165e08d1a5b58a94d7834fe0f6510f2
Josh Cox <josh@webhosting.coop>
2017-03-20 20:30:17 -0500

    host

--- node/start.sh
+++ node/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -l
-: ${BRANCH=master}
-: ${MEANGO_HOST=127.0.0.1}
 
-echo "$MEANGO_HOST meango.meangohost.com meango">>/etc/hosts
+if [ ! -z ${MEANGO_HOST+x} ]
+  then
+    echo "$MEANGO_HOST meango.meangohost.com meango">>/etc/hosts
+fi
+
 npm start

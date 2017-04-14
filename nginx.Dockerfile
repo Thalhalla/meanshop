3ca81ed23c174e110fc7fc281a92947781f87db8
Josh Cox <josh@webhosting.coop>
2017-04-13 23:11:39 -0500

    nginx user and pass

--- nginx.Dockerfile
+++ nginx.Dockerfile
@@ -1,4 +1,10 @@
 FROM nginx:alpine
+
+RUN apk update && \
+apk upgrade && \
+apk add openssl && \
+rm -rf /var/cache/apk/*
+
 COPY nginx /config-nginx
 COPY client /meanshop/current/dist/public
 CMD ["/config-nginx/start.sh"]
--- nginx/start.sh
+++ nginx/start.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -l
 
-if [ ! -z ${NGINX_HTPASSWD+x} ]
+if [ ! -z ${NGINX_PASS+x} ]
   then
     cp /config-nginx/meanshop-password.conf /etc/nginx/conf.d/default.conf
-    echo "$NGINX_HTPASSWD">>/etc/nginx/.htpasswd
+    sudo sh -c "echo -n '$NGINX_USER:' >> /etc/nginx/.htpasswd"
+    sudo sh -c "echo $NGINX_PASS|openssl passwd -apr1 -stdin >>/etc/nginx/.htpasswd"
 else
     cp /config-nginx/meanshop.conf /etc/nginx/conf.d/default.conf
+fi
+if [ ! -z ${NGINX_HTPASSWD+x} ]
+    echo "$NGINX_HTPASSWD">>/etc/nginx/.htpasswd
 fi
 
 nginx -g 'daemon off;'

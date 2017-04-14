b191aca7a3f91eb951b61186a3f9e3962dcd2fe1
Josh Cox <josh@webhosting.coop>
2017-04-14 02:52:53 -0500

    env

--- nginx/start.sh
+++ nginx/start.sh
@@ -3,8 +3,8 @@
 if [ ! -z ${NGINX_PASS+x} ]
   then
     cp /assets/meanshop-password.conf /etc/nginx/conf.d/default.conf
-    sudo sh -c "echo -n '$NGINX_USER:' >> /etc/nginx/.htpasswd"
-    sudo sh -c "echo $NGINX_PASS|openssl passwd -apr1 -stdin >>/etc/nginx/.htpasswd"
+    sudo sh -c "echo -n '${NGINX_USER}:' >> /etc/nginx/.htpasswd"
+    sudo sh -c "echo ${NGINX_PASS}|openssl passwd -apr1 -stdin >>/etc/nginx/.htpasswd"
 else
     cp /assets/meanshop.conf /etc/nginx/conf.d/default.conf
 fi
